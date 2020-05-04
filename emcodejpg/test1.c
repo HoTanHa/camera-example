4a46607452b5035a8dbdfb154f7436b5e2454f18
hotanha <hotanha03021996@gmail.com>
2020-05-04 17:29:42 +0700

    04-05-2020

--- emcodejpg/test1.c
+++ emcodejpg/test1.c
@@ -8,20 +8,18 @@
 
 int main()
 {
-int width, height, channels;
-//unsigned char *img = stbi_load("abc.jpg", &width, &height, &channels, 0);
-unsigned char *img = stbi_load("video0_000.ppm", &width, &height, &channels, 0);
-if(img ==NULL)
-{
-printf("Error in loading the image\n");
-exit(1);
-}
-printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
-stbi_write_png("aas.png", width, height, channels, img, width * channels);
-stbi_write_jpg("sky2.jpg", width, height, channels, img, 100);
+	int width, height, channels;
+	//unsigned char *img = stbi_load("abc.jpg", &width, &height, &channels, 0);
+	unsigned char *img = stbi_load("video0_000.ppm", &width, &height, &channels, 0);
+	if (img == NULL)
+	{
+		printf("Error in loading the image\n");
+		exit(1);
+	}
+	printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
+	stbi_write_png("image_png.png", width, height, channels, img, width * channels);
+	stbi_write_jpg("image_jpg.jpg", width, height, channels, img, 100);
 
-
-return 0;
-
+	return 0;
 }
 //gcc -std=c11 -Wall -pedantic my_program.c -lm -o my_program
